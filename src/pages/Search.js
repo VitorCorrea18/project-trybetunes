3ac2c80091b13ed2c392c03a243be0dc427d87cf
vitorcorrea18 <vitorcorrea18@gmail.com>
2022-01-14 16:23:22 -0400

    requisito 5

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,11 +1,51 @@
 import React from 'react';
 import Header from '../components/Header';
+import '../styles/search.css';
+
+const MIN_SEARCH_LENGTH = 2;
 
 class Search extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      isSearchButtonDisabled: true,
+      searchInput: '',
+    };
+  }
+
+  onInputChange = (event) => {
+    const { name, value } = event.target;
+    this.setState({ [name]: value }, () => {
+      if (value.length >= MIN_SEARCH_LENGTH) {
+        this.setState({ isSearchButtonDisabled:false });
+      } else this.setState({ isSearchButtonDisabled: true });
+    });
+  }
+
   render() {
+    const { isSearchButtonDisabled, searchInput } = this.state;
+
     return (
-      <div data-testid="page-search">
+      <div data-testid="page-search" className="search-page">
         <Header />
+        <form onSubmit={ this.onSubmitButtonClick } className="form-search">
+          <input
+            data-testid="search-artist-input"
+            className="input-search"
+            placeholder="Nome do artista"
+            name="searchInput"
+            value={ searchInput }
+            onChange={ this.onInputChange }
+          />
+          <button
+            data-testid="search-artist-button"
+            type="submit"
+            className="search-button"
+            disabled={ isSearchButtonDisabled }
+          >
+            Procurar
+          </button>
+        </form>
       </div>
     );
   }
